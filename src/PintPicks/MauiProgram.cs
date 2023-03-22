0578fa50bdabe78d12bcc09fb3ea43a1bf822fe7
Miguel <>
2023-03-22 17:41:53 -0400

    PIN-18 Implements Landing Page/Media Picker

--- src/PintPicks/MauiProgram.cs
+++ src/PintPicks/MauiProgram.cs
@@ -4,19 +4,23 @@ namespace PintPicks;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
+
+
+    public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder();
+
+        var builder = MauiApp.CreateBuilder();
+
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
